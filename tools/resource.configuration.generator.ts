d01e654ed1c11bd5e175b7dfb4a29a2d50d2382a
Patrik Forsberg <patfor@gmail.com>
2016-04-08 14:20:43 +0200

    New framework model

---
+++ tools/resource.configuration.generator.ts
@@ -0,0 +1,17 @@
+/**
+ *	Resource Configuration Generator
+ *	@author Patrik Forsberg <patfor@gmail.com>
+ *	@2016-04-07
+ *
+ * 	EXPERIMENTAL DIRECTORY BASED CONFIGURATION BUILDER
+ *
+ */
+class ResourceConfigurationGenerator {
+	constructor() {
+	}
+
+	public buildConfigurationFromDirectoryStructure(rootPath: string, outputConfigFilename): boolean {
+		
+		return true;
+	}
+}
